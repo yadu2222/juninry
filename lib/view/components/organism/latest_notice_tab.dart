c705c8f1f21397aa2351ee5a421977c8f48d8f5f
yadu2222 <ochaocha2004@gmail.com>
2024-06-05 14:52:26 +0900

    [fix]ホームおしらせタブ修正

--- lib/view/components/organism/latest_notice_tab.dart
+++ lib/view/components/organism/latest_notice_tab.dart
@@ -28,7 +28,7 @@ class LatestNoticeTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListItem(
         // height: 130,
-        padding: EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
         // ここに箱の中に表示したいウィジェットを入れる
         widget: Column(
             crossAxisAlignment: CrossAxisAlignment.start, //これをつける
