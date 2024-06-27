666eb77b485e4a7e338f43e4374570e7caffc185
yadu2222 <ochaocha2004@gmail.com>
2024-06-27 11:53:26 +0900

    [req]やさしさのコメント

--- lib/models/req_model.dart
+++ lib/models/req_model.dart
@@ -1,7 +1,7 @@
 class Request {
   String reqType; // 'GET' or 'POST' or 'PUT' or 'DELETE'
   String url;     // base を除いたリクエスト先のURL 直で書かないでね
-  Map<String, String> headers;
-  Map? body;
+  Map<String, String> headers;  // ヘッダー Authorizationは除いてね
+  Map? body;    // body
   Request({required this.reqType, required this.url, this.headers = const {}, this.body});
 }
