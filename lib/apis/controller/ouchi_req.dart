40abda847329f3bdc35d33ee59b6a6e983413d91
yadu2222 <ochaocha2004@gmail.com>
2024-07-19 12:17:59 +0900

    [add]アイコン・ネイティブスプラッシュ

--- lib/apis/controller/ouchi_req.dart
+++ lib/apis/controller/ouchi_req.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
-import '../error.dart';
 // import 'package:go_router/go_router.dart';
 import '../../view/components/atoms/toast.dart';
 import '../../../constant/messages.dart';
-import '../service/user_service.dart';
+import '../service/ouchi_service.dart';
+import '../error.dart';
 
 class OUCHIReq {
   final BuildContext context;
   OUCHIReq(this.context);
+
+
+  // 現在のポイントを取得
+
+  // お手伝いを追加
+
+  // お手伝いを消化
+
+  // お手伝いを削除
+
+  // お手伝いを取得
+
+  // ご褒美を追加
+
+  // ご褒美を交換
+
+  // ご褒美を削除
+
+  // ご褒美を取得
 
  
   
--- lib/apis/service/ouchi_service.dart
+++ lib/apis/service/ouchi_service.dart
@@ -8,5 +8,24 @@ import 'package:http/http.dart' as http;
 import '../error.dart';
 
 class OUCHIService {
+
+  // 現在のポイントを取得
+
+  // お手伝いを追加
+
+  // お手伝いを消化
+
+  // お手伝いを削除
+
+  // お手伝いを取得
+
+  // ご褒美を追加
+
+  // ご褒美を交換
+
+  // ご褒美を削除
+
+  // ご褒美を取得
+
   
 }
